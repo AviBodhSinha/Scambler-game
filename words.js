5197c9e5ef42cc4418a86dad65ecb8c63326a372
AviBodhSinha <137508426+AviBodhSinha@users.noreply.github.com>
2023-06-23 17:38:25 +0530

    Add files via upload

---
+++ words.js
@@ -0,0 +1,35 @@
+const words =[
+{
+    word : "group",
+    hint:"A number of objects or person"
+},
+{
+    word : "addition",
+    hint:"The process of adding numbers"
+},
+{
+    word : "meeting",
+    hint:"Event in which people come together"
+},
+{
+    word : "number",
+    hint:"Math symbol using for counting"
+},
+{
+    word : "exchange",
+    hint:"The act of trading"
+},
+{
+    word : "expansion",
+    hint:"The process of increase or grow"
+},
+{
+    word : "tongue",
+    hint:"The muscular organ of mouth"
+},
+{
+    word : "comfort",
+    hint:"A pleasant feeling of relaxation"
+},
+
+]
